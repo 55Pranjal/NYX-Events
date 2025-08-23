cf039fa20e1999b0ecec1a544bfb0b8406bf8dcc
55Pranjal <567pranjalagarwal@gmail.com>
2025-08-24 01:18:31 +0530

    hey

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -18,7 +18,7 @@ const SignUpScreen = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_BACKEND_URL}/api/users/add`,
+        `https://backend-nyx-9.onrender.com/api/users/add`,
         {
           name,
           email,
