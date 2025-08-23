fa7136c30217ae665136e2d944d60987b54877e7
55Pranjal <567pranjalagarwal@gmail.com>
2025-08-24 01:12:54 +0530

    hey

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -18,7 +18,7 @@ const SignUpScreen = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/users/add`,
+        `${import.meta.env.VITE_BACKEND_URL}/api/users/add`,
         {
           name,
           email,
